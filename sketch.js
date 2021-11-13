74a5c84b457ffde317fdad04143fd612b4cef323
Glitch (glitch-blank-website) <none>
2021-11-13 20:55:20 +0000

    👁💫 Checkpoint ./sketch.js:38704244/537

--- sketch.js
+++ sketch.js
@@ -1,39 +1,28 @@
 let r, g, b;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); //sets canvas to fill entire screen
   noStroke();
-  //g = random(255);
   frame = 0;
 }
 
 function draw() {
-//   if (mouseIsPressed) {
-//     fill(0);
-//   } else {
-//     fill(255);
-//   }
-//   ellipse(mouseX, mouseY, 80, 80);
   background(0);
   
-  g = floor(map(cos(frameCount/100), -1, 1, 0, 255));
-  b = floor(sin(frameCount/100), -1, 1, 0, 255));
+  g = floor(map(cos(frameCount/100), -1, 1, 0, 255)); //g value is cos of frames
+  b = floor(map(sin(frameCount/100), -1, 1, 0, 255)); //b value is sine of frames
   
-  for(let x = 0; x < width; x += 10){
-    r = map(x, 0, width, 0, 255);
+  for(let x = 0; x < width; x += 10){ //loops through canvas, draws little squares
     for(let y = 0; y < height; y+=10){
-    //let g = map(y, 0, height, 0, 255);
-    fill(r,g,b);
-    rect(x, y, width/100, width/100);
+      r = map(y, 0, width, 255, 0);
+      //let g = map(y, 0, height, 0, 255);
+      fill(r,g,b);
+      rect(x, y, 10, 10);
     }
   }
   
-
-  fill(255);
-  text(g, 20, 20);
-  text(b, 20, 40);
-  //loop over height
-  
-  //increase blue value
+//   fill(255);
+//   text(g, 20, 20);
+//   text(b, 20, 40);
   
 }
