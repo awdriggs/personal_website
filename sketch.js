beebabdfabfc0147441fc1b0b9fdde4686f57122
Glitch (glitch-blank-website) <none>
2021-11-13 20:20:21 +0000

    👶🖐 Checkpoint ./sketch.js:38704244/845

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
-function setup() {
+let r, g, b;
 
+function setup() {
   createCanvas(windowWidth, windowHeight);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+//   if (mouseIsPressed) {
+//     fill(0);
+//   } else {
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+  background(0);
+  
+  for(let x = 0; x < width; x += 10){
+    for(let y = 0; y < height; y+=10){
+    fill(r,g,b);
+    rect(x, y, width/100, width/100);
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  //loop over height
+  
+  //increase blue value
+  
 }
