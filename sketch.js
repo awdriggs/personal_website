61aba2c0f39770811a261bd4a921e9642530c619
Glitch (glitch-blank-website) <none>
2021-11-13 20:43:22 +0000

    🚓🍯 Checkpoint ./sketch.js:38704244/433 ./style.css:38704244/25

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let r, g, b;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  g = random(255);
+  //g = random(255);
   frame = 0;
 }
 
@@ -16,8 +16,11 @@ function draw() {
 //   ellipse(mouseX, mouseY, 80, 80);
   background(0);
   
+  g = floor(map(cos(frameCount/100), -1, 1, 0, 255));
+  b = floor(sin(frameCount/100), -1, 1, 0, 255));
+  
   for(let x = 0; x < width; x += 10){
-    let r = map(x, 0, width, 0, 255);
+    r = map(x, 0, width, 0, 255);
     for(let y = 0; y < height; y+=10){
     //let g = map(y, 0, height, 0, 255);
     fill(r,g,b);
@@ -25,8 +28,10 @@ function draw() {
     }
   }
   
-  b = map(sin(frameCount/100), -1, 1, 0, 255);
 
+  fill(255);
+  text(g, 20, 20);
+  text(b, 20, 40);
   //loop over height
   
   //increase blue value
